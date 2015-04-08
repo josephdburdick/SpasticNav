e074afb4a6d9f2574aa9c61d4c3caffaf38d63fb
Joe <josephdburdick@gmail.com>
2015-04-08 16:44:34 -0400

    Allow no #nav-primary-selected id, choose first by default and track user mouseenter.

--- js/jquery.spasticNav.js
+++ js/jquery.spasticNav.js
@@ -13,65 +13,66 @@
 		return this.each(function() {
 		
 		 	var nav = $(this),
-		 		currentPageItem = $('#nav-primary-selected', nav),
+		 		isSelected = $('#nav-primary-selected', nav).length ? true : false,
+		 		currentPageItem = (function(){
+		 			// if primary nav item selected use it
+		 			if ($('#nav-primary-selected', nav).length){
+		 				return $('#nav-primary-selected', nav);
+		 			} else {
+						return nav.find('li').first();	
+		 			}
+		 		})(),
 		 		blob,
-		 		reset;
-		 		
+		 		reset,
+		 		transition;
+
 		 	$('<li id="primary-nav-blob"></li>').css({
 		 		width : currentPageItem.outerWidth(),
 		 		height : currentPageItem.outerHeight() + options.overlap,
-		 		left : currentPageItem.position().left,
-		 		top : currentPageItem.position().top - options.overlap / 2
+		 		left : isSelected ? currentPageItem.position().left : $(this).position().left,
+		 		top : currentPageItem.position().top - options.overlap / 2,
+		 		opacity: isSelected ? 1 : 0
 		 	}).appendTo(this);
 		 	
 		 	blob = $('#primary-nav-blob', nav);
 					 	
 			$('li:not(#primary-nav-blob)', nav).hover(function() {
+				
 				// mouse over
+				transition = {
+					left : $(this).position().left,
+					width : $(this).width(),
+					opacity: 1
+				},
+				{
+					duration : options.speed,
+					easing : options.easing,
+					queue : false
+				};
+
 				clearTimeout(reset);
-				if(!Modernizr.csstransitions){
-					blob.animate(
-						{
-							left : $(this).position().left,
-							width : $(this).width()
-						},
-						{
-							duration : options.speed,
-							easing : options.easing,
-							queue : false
-						}
-					);
-				} else {
-					blob.css(
-						{
-							left : $(this).position().left,
-							width : $(this).width()
-						},
-						{
-							duration : options.speed,
-							easing : options.easing,
-							queue : false
-						}
-					);
-				}
+				if(!Modernizr.csstransitions)
+					blob.animate(transition);
+				else
+					blob.css(transition);
+
 			}, function() {
+
 				// mouse out	
+				transition = {
+					width : currentPageItem.outerWidth(),
+					left : isSelected ? currentPageItem.position().left : $(this).position().left,
+					opacity: isSelected ? 1 : 0
+				}, options.speed;
+
 				reset = setTimeout(function() {
-					if(!Modernizr.csstransitions){
-						blob.animate({
-							width : currentPageItem.outerWidth(),
-							left : currentPageItem.position().left
-						}, options.speed);
-					} else {
-						blob.css({
-							width : currentPageItem.outerWidth(),
-							left : currentPageItem.position().left
-						}, options.speed);
-					}
+					if(!Modernizr.csstransitions)
+						blob.animate(transition);
+					else
+						blob.css(transition);
 				}, options.reset);
 				
 			});
-		 
 		
 		}); // end each
 	
